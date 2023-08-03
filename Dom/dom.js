878b03e43ec76c5c62ad0fcdcafb848da52ce47f
Naresh Khatri <111528185+nareshtestgithub@users.noreply.github.com>
2023-08-03 11:21:28 +0545

    Dom manipulation

---
+++ Dom/dom.js
@@ -0,0 +1,21 @@
+// this is the process of selecting the element..
+var a = document.querySelector('h1')
+console.log(a)
+
+
+// changing html contnents..
+
+var p = document.querySelector('p')
+p.innerHTML = "This is the example of changing the html contents..";
+
+
+
+// changing css ....
+
+var span = document.querySelector('span')
+span.style.color = 'red';
+span.style.backgroundColor = "Black";
+span.style.border = '2px solid black';
+span.style.fontSize ='3rem'
+
+
--- first.js
+++ first.js
@@ -3,3 +3,4 @@ console.log( 5 + "naresh");
 console.log(123 / "3"+5);
 console.log("Hello world");
 console.log(12 + 12 );
+document.querySelector
