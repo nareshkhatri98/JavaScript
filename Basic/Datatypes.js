99899b361b02b4b0e3be6f94ce31b85da897e95e
Naresh Khatri <111528185+nareshtestgithub@users.noreply.github.com>
2023-12-31 19:09:26 +0545

    Data Types

---
+++ Basic/Datatypes.js
@@ -0,0 +1,13 @@
+"use strict"; // This keyword provides the version of the javascript
+
+let name = "Naresh Khatri"// The data types of this variable is string
+let age  = 20 // The data types of the  age variable is intiger / Number.
+let LogIN = false // the data types of theis variable is boolena . It has two choice true or false.
+let address;// Data type id undefined
+let temp = null // Data type is object
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof LogIN);
+console.log(typeof address);
+console.log(typeof temp);
